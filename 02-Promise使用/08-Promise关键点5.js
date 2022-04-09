300867125e01d02c0007607a23be8e381098ae73
lesenelir <miaomiaobabyzy@gmail.com>
2022-04-09 23:04:51 +0800

    udpate promise

--- 02-Promise使用/08-Promise关键点5.js
+++ 02-Promise使用/08-Promise关键点5.js
@@ -6,6 +6,7 @@
  *  7. 中断Promise链
  *    （1）当使用Promise的then链式调用时，在中间中断，不再调用后面的回调函数
  *    （2）办法：在回调函数中返回一个pending状态的Promise对象
+ *             new Promise(() => {})
  *
  */
 
@@ -17,7 +18,8 @@ new Promise((resolve, reject) => {
       console.log('onResolved1()', value)
       return 2
     },
-    // reason => {throw reason}
+    // reason => {throw reason} // 没写失败的回调函数，相当于写这一句话
+    // reason => Promise.reject(reason)
 ).then(
     value => {
       console.log('onResolved2()', value)
@@ -35,8 +37,7 @@ new Promise((resolve, reject) => {
   console.log('onReejected1()', reason)
   // throw reason
   // return Promise.reject(reason)
-  return new Promise(() => {
-  }) // 返回一个pending的promise  中断promise链
+  return new Promise(() => {}) // 返回一个pending的promise  中断promise链 - 后面的then不会执行
 }).then(
     value => {
       console.log('onResolved3()', value)
